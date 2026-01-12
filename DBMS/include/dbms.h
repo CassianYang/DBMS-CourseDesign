3cac2bfff9a23525858d4d70bec9647de8d42cb7
CassianYang <2941416693@qq.com>
2026-01-12 16:05:09 +0800

    上传测试文件

---
+++ DBMS/include/dbms.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class DBMS
+{
+public:
+    DBMS();
+    void print_menu();
+    ~DBMS();
+};
---
+++ DBMS/test/dbms_test.cpp
@@ -0,0 +1,8 @@
+#include "dbms.h"
+
+int main()
+{
+    DBMS dbms;
+    dbms.print_menu();
+    return 0;
+}
